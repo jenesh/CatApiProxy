561e16fd35f36b15d5fb01e3bd2cea19c159a776
Jenesh <jenesh.napit@gmail.com>
2019-10-20 10:48:00 -0400

    Cat Api Proxy to get random cat image

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
+
+const apiKey = require('./apiKey/catApi')
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.u
+
+app.get('/', (req, res) => {
+    res.json(apiKey);
+})
+
+app.get('/random', async (req, res) => {
+
+    const config = {
+        method: 'get',
+        url: 'https://api.thecatapi.com/v1/images/search',
+        headers: {'x-api-key' : apiKey}
+    }
+
+    const data = await axios(config);
+
+    res.send(data.data);
+    console.log(data);
+})
+
+
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+})
